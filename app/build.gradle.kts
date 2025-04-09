21396f607c1477d2dd329656304d794411826aae
Kunal Farmah <kunalfarmah98@gmail.com>
2025-04-09 22:23:06 +0530

    feat: Bump version to 1.2
    - Updated `versionCode` to 3.
    - Updated `versionName` to "1.2".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.apps.kunalfarmah.k_gpt"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GEMINI_API_KEY", geminiApiKey)
